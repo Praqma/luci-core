830272e382348c72ee50351b32471acddf3d2e9c
Jan Sorensen <jas@praqma.net>
2015-09-03 05:22:35 +0200

    Restructure the initializing of Images class

--- src/main/groovy/net/praqma/luci/docker/Images.groovy
+++ src/main/groovy/net/praqma/luci/docker/Images.groovy
@@ -19,19 +19,20 @@ enum Images {
 
     TOOLS('luci/tools')
 
-
     final String imageString
 
     Images(String imageString) {
         this.imageString = ensureVersion(imageString)
     }
 
-    private String ensureVersion(String imageString) {
+    private static Map<String, String> m
+
+    private static String ensureVersion(String imageString) {
         String prefix = 'luci/'
-        versionMap() != null
+        if (m == null) m = versionMap()
         if (imageString.startsWith(prefix)) {
             if (imageString.indexOf(':') == -1) {
-                String version = versionMap()[imageString.substring(prefix.length())]
+                String version = m[imageString.substring(prefix.length())]
                 if (version == null) {
                     throw new RuntimeException("No version defined for '${imageString}")
                 }
@@ -41,12 +42,15 @@ enum Images {
         return imageString
     }
 
-    @Memoized
-    private Map<String, String> versionMap() {
-        InputStream stream = Thread.currentThread().contextClassLoader.getResourceAsStream('docker/imageVersions.properties')
-        assert stream != null
+    private static Map<String, String> versionMap() {
+        String resource = 'docker/imageVersions.properties'
+        InputStream stream = Images.classLoader.getResourceAsStream(resource)
+        if (stream == null) {
+            throw new RuntimeException("Luci: Internal error: Could not read image versions from ${resource}")
+        }
         Properties answer = new Properties()
         answer.load(stream)
         return answer
     }
+
 }
---
+++ src/test/groovy/net/praqma/luci/docker/ImagesTest.groovy
@@ -0,0 +1,11 @@
+package net.praqma.luci.docker
+
+import org.junit.Test
+
+class ImagesTest {
+
+    @Test
+    void testLoadImagesClass() {
+        Images.DATA
+    }
+}
