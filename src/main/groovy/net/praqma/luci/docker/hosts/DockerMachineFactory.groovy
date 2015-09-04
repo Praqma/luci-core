8dd518d737ac718ad646318a648241cf391f8619
Jan Sorensen <jas@praqma.net>
2015-09-04 21:08:28 +0200

    More flexibility in defining machine factories

--- src/main/groovy/net/praqma/luci/docker/hosts/DockerMachineFactory.groovy
+++ src/main/groovy/net/praqma/luci/docker/hosts/DockerMachineFactory.groovy
@@ -1,9 +1,11 @@
 package net.praqma.luci.docker.hosts
 
+import com.google.common.annotations.VisibleForTesting
 import groovy.text.SimpleTemplateEngine
 import groovy.text.TemplateEngine
 import net.praqma.luci.docker.DockerHost
 import net.praqma.luci.utils.ExternalCommand
+import net.praqma.luci.utils.LuciSettings
 
 /**
  * Specify a factory to create docker machines.
@@ -15,29 +17,64 @@ class DockerMachineFactory {
     /**
      * Arguments for the 'docker-machine create' command
      */
-    List<String> createArgs = []
+    List<String> createArgs = ['$name']
+
+    Map<String, String> options = [:]
+
+    String driver
+
+    /** Execute docker-machine create with debug flag? */
+    boolean debug = false
+
+    Map<String, String> bindings = [:]
 
     DockerMachineFactory(String name) {
         this.name = name
     }
 
-    DockerHost getOrCreate(String machineName) {
-        List cmd = ['docker-machine', 'create']
-        Map<String, String> bindings = [ name: machineName]
+    void options(Map<String, String> opts) {
+        options.putAll(opts)
+    }
+
+    void driver(String driver) {
+        this.driver = driver
+    }
+
+    @VisibleForTesting
+    List<String> commandLine(String machineName) {
+        Map<String, String> binds = [ name: machineName ]
+        binds.putAll(bindings)
         TemplateEngine engine = new SimpleTemplateEngine()
+        def x = { String input -> engine.createTemplate(input).make(binds).toString() }
+
+        List<String> cmd = ['docker-machine']
+        if (debug) cmd << '--debug'
+        cmd << 'create'
+        cmd.addAll(buildCompleteOptionsMap().collect { key, value -> ["--${key}".toString(), x(value)] }.flatten())
         createArgs.each { String arg ->
-            String expanded = engine.createTemplate(arg).make(bindings).toString()
-            cmd << expanded
+            assert arg != null
+            cmd << x(arg)
         }
+        return cmd
+    }
+
+    DockerHost getOrCreate(String machineName) {
         StringBuffer err = "" << ""
         println "Creating machine: '${machineName}'"
-        int rc = new ExternalCommand().execute(*cmd, err: err)
+        int rc = new ExternalCommand().execute(commandLine(machineName, LuciSettings.instance), err: err)
         if (rc == 0) {
             return new DockerMachineHost(machineName)
         } else {
             // TODO check if exist
             throw new RuntimeException(err.toString())
         }
+    }
+
+    private Map<String, String> buildCompleteOptionsMap() {
+        Map<String, String> answer = [:]
+        if (driver) answer['driver'] = driver
+        answer.putAll(options)
+        return answer
     }
 
 }
--- src/test/groovy/net/praqma/luci/docker/DockerMachineFactoryTest.groovy
+++ src/test/groovy/net/praqma/luci/docker/DockerMachineFactoryTest.groovy
@@ -1,5 +1,55 @@
 package net.praqma.luci.docker
 
+import net.praqma.luci.docker.hosts.DockerMachineFactory
+import org.junit.Test
+
 
 class DockerMachineFactoryTest {
+
+    @Test
+    void testDebugFlag() {
+        DockerMachineFactory f = new DockerMachineFactory()
+        assert f.commandLine('test')[0..1] == ['docker-machine', 'create']
+        f.debug = true
+        assert f.commandLine('test')[0..2] == ['docker-machine', '--debug', 'create']
+    }
+
+    @Test
+    void testNameTemplating() {
+        DockerMachineFactory f = new DockerMachineFactory()
+        assert f.commandLine('dummy') == ['docker-machine', 'create', 'dummy']
+    }
+
+    @Test
+    void testDriver() {
+        DockerMachineFactory f = new DockerMachineFactory()
+        f.driver = "foo"
+        f.options 'opt-key': 'opt-value'
+        f.createArgs << 'bar'
+        assert f.commandLine('dummy') == ['docker-machine', 'create', '--driver', 'foo', '--opt-key', 'opt-value', 'dummy', 'bar']
+    }
+
+    @Test
+    void testBindings() {
+        DockerMachineFactory f = new DockerMachineFactory()
+        // Create a binding to a map
+        f.bindings.settings = ['zetta.username': 'abc']
+        // X uses the settings map to lookup value
+        f.options x: '${settings["zetta.username"]}'
+        assert f.commandLine('mach') == ['docker-machine', 'create', '--x', 'abc', 'mach']
+    }
+
+    @Test
+    void testLookupFunction() {
+        DockerMachineFactory f = new DockerMachineFactory()
+        // Create a binding to a function
+        f.bindings.lookup = { key ->
+            [ one: '1', two: '2'][key] ?: { throw new RuntimeException("${key} is not defined")}()
+        }
+        f.options x: '${lookup("one")}'
+        assert f.commandLine('mach') == ['docker-machine', 'create', '--x', '1', 'mach']
+    }
+
+
+
 }
