55ef8a53ef2db4dd39e425b3b0718e10a32cb7c0
Jan Sorensen <jas@praqma.net>
2015-09-07 11:02:36 +0200

    Use port in slaveConnect.sh

--- src/main/resources/scripts/connectSlave.sh
+++ src/main/resources/scripts/connectSlave.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 
 slaveName=$1
+port=$2
 
 slaveJar=/luci/data/jenkinsSlave/slave.jar
 
+jnlpUrl=http://lucibox:$port/jenkins/computer/$slaveName/slave-agent.jnlp
+
+echo "jnlpUrl: $jnlpUrl"
+
 while true ; do
   echo "Connecting to master"
-  /luci/mixins/java/bin/java -jar $slaveJar -jnlpUrl http://lucibox/jenkins/computer/$slaveName/slave-agent.jnlp
+  /luci/mixins/java/bin/java -jar $slaveJar -jnlpUrl $jnlpUrl
   echo "Connection to Master terminated! Reconnect attempt in 2 minutes "
   # Retry logic is also built into slave.jar. It is quite exceptional to end up here
   sleep 120
